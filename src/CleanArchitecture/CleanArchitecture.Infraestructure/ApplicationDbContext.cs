7d33606d6a6dbe9d9ff624b29afe486436dcbca6
lumelamed <lumelamed@gmail.com>
2024-07-17 15:37:05 -0300

    agrego librerias de Entity Framework y creo ApplicationDbContext

---
+++ src/CleanArchitecture/CleanArchitecture.Infraestructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArchitecture.Infrastructure
+{
+    using CleanArchitecture.Domain.Abstractions;
+    using Microsoft.EntityFrameworkCore;
+
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
